ef82523db297cf15ebff157069282b13ddb068cc
ZHines2 <zhines2@gmail.com>
2025-02-13 17:30:15 -0500

    Update levels.js
    stonch hogan

--- levels.js
+++ levels.js
@@ -185,9 +185,9 @@ export const levelSettings = [
       { name: "Brigand", symbol: "Җ", attack: 3, range: 1, hp: 12, agility: 2, x: 1, y: 4 },
       { name: "Brigand", symbol: "Җ", attack: 3, range: 1, hp: 12, agility: 2, x: 2, y: 4 },
       { name: "Buckleman", symbol: "⛨", attack: 1, range: 1, hp: 20, agility: 1, x: 3, y: 4 },
-      { name: "New Enemy", symbol: "N", attack: 2, range: 1, hp: 10, agility: 2, x: 4, y: 6 },
-      { name: "New Enemy", symbol: "N", attack: 2, range: 1, hp: 10, agility: 2, x: 5, y: 6 },
-      { name: "New Enemy", symbol: "N", attack: 2, range: 1, hp: 10, agility: 2, x: 6, y: 6 }
+      { name: "Getter", symbol: "∴", attack: 5, range: 1, hp: 50, agility: 5, x: 4, y: 6 },
+      { name: "Stonch Hogan", symbol: "酉", attack: 7, range: 1, hp: 100, agility: 3, x: 5, y: 6 },
+      { name: "Taker", symbol: "∵", attack: 1, range: 5, hp: 50, agility: 5, x: 6, y: 6 }
     ]
   },
   {
